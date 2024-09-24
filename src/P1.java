bbb762ec884f5e0eca694cb62059b6a839872adf
Peter <peter94@nycstudents.net>
2024-09-24 13:19:47 -0400

    done with p1

--- src/P1.java
+++ src/P1.java
@@ -4,6 +4,7 @@ public class P1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // inputs
         System.out.print("Enter your bill: $");
         double bill = scanner.nextDouble();
 
@@ -13,19 +14,35 @@ public class P1 {
         System.out.print("Number of people: ");
         int numOfPeople = scanner.nextInt();
 
+        // calculate everything
         double tip = (tipPercent / 100) * bill;
         double totalBill = bill + tip;
         double tipPerPerson = tip / numOfPeople;
         double totalBillPerPerson = totalBill / numOfPeople;
 
-        printLine("Total tip: $" + tip);
+        // print everything
+        printLine("Total tip: $" + tip, false);
+        printLine("Total bill cost: $" + totalBill, false);
+        printLine("Tip per person: $" + tipPerPerson, false);
+        printLine("Total bill per person: $" + totalBillPerPerson, true);
     }
 
-    public static void printLine(String out) {
-        // TODO: make this work properly
-        System.out.println("------------------------------");
+    /**
+     * Prints a line as needed for this project.
+     *
+     * @param out The string to print.
+     * @param close Whether to close this printed line with a closing line or not.
+     */
+    public static void printLine(String out, boolean close) {
+        String line = "-".repeat(out.length() + 4);
+
+        System.out.println(line);
         System.out.print("| ");
         System.out.print(out);
         System.out.println(" |");
+
+        if (close) {
+            System.out.println(line);
+        }
     }
 }
